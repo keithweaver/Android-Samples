362cc178f507eb0955db991cf3fd4561bd95f4c6
Keith Weaver <keithmweaver@gmail.com>
2015-02-20 18:47:03 -0500

    basic map

---
+++ PushNotifApp/app/src/main/java/com/weaverprojects/pushnotifapp/ParsePushApplication.java
@@ -0,0 +1,14 @@
+package com.weaverprojects.pushnotifapp;
+
+import android.app.Application;
+import com.parse.Parse;
+import com.parse.PushService;
+/**
+ * Created by Weaver on 15-02-20.
+ */
+public class ParsePushApplication extends Application {
+    public void onCreate(){
+        Parse.initialize(this, getString(R.string.parseAppId), getString(R.string.parseClientId));
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+    }
+}
