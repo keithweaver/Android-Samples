883f8800737bb6ca707e2b3988aaa3228099e8cd
Keith Weaver <keithmweaver@gmail.com>
2015-03-05 12:04:46 -0500

    just ran a test.

--- Point/app/src/main/java/com/weaverprojects/point/friends_list.java
+++ Point/app/src/main/java/com/weaverprojects/point/friends_list.java
@@ -6,18 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
-
 /**
  * Created by Weaver on 15-03-05.
  */
 public class friends_list extends Activity{
     public void onCreate(Bundle savedInstanceState){
         // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
+        //Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this, "ACnXHKlS8m40Xppjus7W21FJes5Fjw7cKdxA2sBX", "XxPYpJBO1XC3yqMs0l1FAUvXpM3wYqN8E7Gjf2Pq");
+        //Parse.initialize(this, "ACnXHKlS8m40Xppjus7W21FJes5Fjw7cKdxA2sBX", "XxPYpJBO1XC3yqMs0l1FAUvXpM3wYqN8E7Gjf2Pq");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_list);
@@ -25,9 +22,9 @@ public class friends_list extends Activity{
 
 
 
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        //ParseObject testObject = new ParseObject("TestObject");
+        //testObject.put("foo", "bar");
+        //testObject.saveInBackground();
 
 
         /* --------------------- */
