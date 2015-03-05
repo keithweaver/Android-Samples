e075598046d7a976b9c487300f7f099f89a9d532
Keith Weaver <keithmweaver@gmail.com>
2015-03-05 18:17:58 -0500

    List view got working.

---
+++ ListViewTest/app/src/main/java/com/weaverprojects/listviewtest/MainActivity.java
@@ -0,0 +1,62 @@
+package com.weaverprojects.listviewtest;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ListActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            setContentView(R.layout.activity_main);//WHY!! DOES THIS CAUSE AN ERROR!
+        }catch(Exception e){
+            Log.v("Tag1:",e.toString());
+        }
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[]{"Keith Weaver","Scott Weaver","Dana Weaver"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) listView.getItemAtPosition(position);
+                Log.v("Test", itemValue);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- Point/app/src/main/java/com/weaverprojects/point/add_friend.java
+++ Point/app/src/main/java/com/weaverprojects/point/add_friend.java
@@ -2,6 +2,7 @@ package com.weaverprojects.point;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,19 +16,27 @@ public class add_friend extends ListActivity{
     ArrayList<String> list = new ArrayList<String>();
     //ListView listview;
     /** Declaring an ArrayAdapter to set items to ListView */
-    //ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_friend);
-
+        try {
+            setContentView(R.layout.add_friend);
+        }catch(Exception e){
+            Log.v("Page Error:", "Doesn't load");
+        }
         //listview = (ListView) findViewById(R.id.listView);
-        list.add("Keith Weaver");
+
+        list.add("keweav");
+
         //adapter = new ArrayAdapter<String>(this, R.layout.add_friend_row, R.id.text1, list);
         //adapter.notifyDataSetChanged();
         //listview.setListAdapter(adapter);
-        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this, R.layout.add_friend_row, R.id.text1, list);
-        ListView lv = (ListView)findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.add_friend_row, R.id.text1, list);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(adapter);
+
     }
 
 }
