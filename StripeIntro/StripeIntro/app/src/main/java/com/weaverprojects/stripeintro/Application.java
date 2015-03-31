7f546ce1fe7a8f90aaebc407fced32bb6ed1d9a2
Keith Weaver <keithmweaver@gmail.com>
2015-03-31 09:26:04 -0400

    started to test integrating stripe into applications

---
+++ StripeIntro/StripeIntro/app/src/main/java/com/weaverprojects/stripeintro/Application.java
@@ -0,0 +1,10 @@
+package com.weaverprojects.stripeintro;
+
+/**
+ * Created by Weaver on 15-03-29.
+ */
+public class Application extends android.app.Application {
+    public void onCreate(){
+
+    }
+}
