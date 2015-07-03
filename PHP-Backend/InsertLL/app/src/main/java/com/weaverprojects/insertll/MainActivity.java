fe83bd5bc1c2a18bb0e288fad398bc9817526f53
Keith Weaver <keithmweaver@gmail.com>
2015-07-02 20:17:30 -0400

    php backend for location

--- PHP-Backend/InsertLL/app/src/main/java/com/weaverprojects/insertll/MainActivity.java
+++ PHP-Backend/InsertLL/app/src/main/java/com/weaverprojects/insertll/MainActivity.java
@@ -1,21 +1,35 @@
 package com.weaverprojects.insertll;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+    public static TextView textView;
+    Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = this.getApplicationContext();
+        textView = (TextView) findViewById(R.id.textView);
+
         Button insertButton = (Button) findViewById(R.id.insertBtn);
-        
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double latitude = 75.00004;
+                double longitude = 140.000010;
+                new LoadInfoActivity(mContext, textView).execute(String.valueOf(latitude), String.valueOf(longitude));
+            }
+        });
     }
 
     @Override
