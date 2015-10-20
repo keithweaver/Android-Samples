40cdc1b8f37ff69f49322a40e2e5c283aaf62bb9
Keith Weaver <keithmweaver@gmail.com>
2015-10-20 07:43:25 -0400

    Pull down refresh example

---
+++ UI/PullToRefresh/app/src/main/java/com/weaverprojects/pulltorefresh/MainActivity.java
@@ -0,0 +1,61 @@
+package com.weaverprojects.pulltorefresh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
+
+    SwipeRefreshLayout swipeLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        swipeLayout.setOnRefreshListener(this);
+        swipeLayout.setColorScheme(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeLayout.setRefreshing(false);
+            }
+        }, 5000);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent openListViewWindow = new Intent(this, ListViewActivity.class);
+            startActivity(openListViewWindow);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
