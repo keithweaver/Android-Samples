ae8aad840f266dfdd91f82e7df7822b2c3dd4526
Keith Weaver <keithmweaver@gmail.com>
2015-10-28 21:04:57 -0400

    Swipeable row example....its done so none is my code

---
+++ sandbox/RowSwipeApp2/app/src/main/java/com/weaverprojects/rowswipeapp2/MainActivity.java
@@ -0,0 +1,120 @@
+package com.weaverprojects.rowswipeapp2;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.wdullaer.swipeactionadapter.SwipeActionAdapter;
+import com.wdullaer.swipeactionadapter.SwipeDirections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ListActivity implements
+        SwipeActionAdapter.SwipeActionListener
+{
+    protected SwipeActionAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] content = new String[20];
+        for (int i=0;i<20;i++) content[i] = "Row "+(i+1);
+        ArrayAdapter<String> stringAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.single_row,
+                R.id.text,
+                new ArrayList<String>(Arrays.asList(content))
+        );
+        mAdapter = new SwipeActionAdapter(stringAdapter);
+        mAdapter.setSwipeActionListener(this)
+                .setDimBackgrounds(true)
+                .setListView(getListView());
+        setListAdapter(mAdapter);
+
+        mAdapter.addBackground(SwipeDirections.DIRECTION_FAR_LEFT,R.layout.row_bg_left_far)
+                .addBackground(SwipeDirections.DIRECTION_NORMAL_LEFT,R.layout.row_bg_left)
+                .addBackground(SwipeDirections.DIRECTION_FAR_RIGHT,R.layout.row_bg_right_far)
+                .addBackground(SwipeDirections.DIRECTION_NORMAL_RIGHT,R.layout.row_bg_right);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView listView, View view, int position, long id){
+        Toast.makeText(
+                this,
+                "Clicked "+mAdapter.getItem(position),
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+    @Override
+    public boolean hasActions(int position){
+        return true;
+    }
+
+    @Override
+    public boolean shouldDismiss(int position, int direction){
+        return direction == SwipeDirections.DIRECTION_NORMAL_LEFT;
+    }
+
+    @Override
+    public void onSwipe(int[] positionList, int[] directionList){
+        for(int i=0;i<positionList.length;i++) {
+            int direction = directionList[i];
+            int position = positionList[i];
+            String dir = "";
+
+            switch (direction) {
+                case SwipeDirections.DIRECTION_FAR_LEFT:
+                    dir = "Far left";
+                    break;
+                case SwipeDirections.DIRECTION_NORMAL_LEFT:
+                    dir = "Left";
+                    break;
+                case SwipeDirections.DIRECTION_FAR_RIGHT:
+                    dir = "Far right";
+                    break;
+                case SwipeDirections.DIRECTION_NORMAL_RIGHT:
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Test Dialog").setMessage("You swiped right").create().show();
+                    dir = "Right";
+                    break;
+            }
+            Toast.makeText(
+                    this,
+                    dir + " swipe Action triggered on " + mAdapter.getItem(position),
+                    Toast.LENGTH_SHORT
+            ).show();
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+}
