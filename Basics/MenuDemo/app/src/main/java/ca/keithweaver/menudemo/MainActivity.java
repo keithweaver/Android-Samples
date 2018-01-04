4c1cd78f08eeb7e33f17a67566e1c43bd27524e1
Keith Weaver <keithmweaver@gmail.com>
2018-01-04 17:38:56 -0500

    Toolbar title

--- Basics/MenuDemo/app/src/main/java/ca/keithweaver/menudemo/MainActivity.java
+++ Basics/MenuDemo/app/src/main/java/ca/keithweaver/menudemo/MainActivity.java
@@ -2,6 +2,7 @@ package ca.keithweaver.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Test");
+        setSupportActionBar(myToolbar);
     }
 }
