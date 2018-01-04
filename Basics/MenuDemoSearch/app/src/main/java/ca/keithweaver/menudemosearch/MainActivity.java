d992d0e69b711aa0fd03d963e0f1ac5329ab7d28
Keith Weaver <keithmweaver@gmail.com>
2018-01-04 17:46:35 -0500

    Starting point for menu bar with search

---
+++ Basics/MenuDemoSearch/app/src/main/java/ca/keithweaver/menudemosearch/MainActivity.java
@@ -0,0 +1,18 @@
+package ca.keithweaver.menudemosearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Test");
+        setSupportActionBar(myToolbar);
+    }
+}
